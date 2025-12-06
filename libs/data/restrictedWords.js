36495745e4e58e6e858b9902fc6e6886ace9ac28
VeronaDanzka <davide.denotarpietro@gmail.com>
2025-12-06 04:23:28 +0100

    contenu restrictedWords

---
+++ libs/data/restrictedWords.js
@@ -0,0 +1,64 @@
+// liste d’expressions sensibles — les plus longues d’abord
+export const REMOVE   = ['VELCRO', 'SEXPLORE', 'LUSH', "TENGA", 
+  "SATISFYER"];
+export const REPLACE = [
+  'MAGIC WAND', 'COCK RING', 'SEX TOY', 'TOY KIT',
+  'MINI VIBRATOR', 'DOG_DILDO', 'PENIS SLEEVE', 'ANAL PLUG',
+  'URETHRAL SOUND', 'DEVIL RING COCK', 'POINT G',
+  // mots simples
+  'SEX', 'VIBRATOR', 'COCK', 'DICK', 'MASTURBATOR', 'VAGINA',
+  'CLITORIS', 'RABBIT', 'BUTT', 'ASS', 'DILDO', 'PENIS', 'ANAL', 'ANTIBACTERIAL', 'URETHRAL',
+  'COCKRING', 'MALTESE DOG',
+  // expressions composées
+  'BAGUETTE MAGIQUE',       // MAGIC WAND
+  'ANNEAU PÉNIEN',          // COCK RING
+  'JOUET SEXUEL',           // SEX TOY
+  'KIT SEXTOY',             // TOY KIT
+  'MINI VIBRO',             // MINI VIBRATOR
+  'GODE CHIEN',             // DOG_DILDO
+  'SLEEVE PÉNIS',           // PENIS SLEEVE
+  'PLUG ANAL',              // ANAL PLUG
+  'SONDE URÉTHRALE',        // URETHRAL SOUND
+  'ANNEAU PÉNIEN DEVIL',    // DEVIL RING COCK
+  // mots simples
+  'SEXE',           // SEX
+  'VIBRO',          // VIBRATOR
+  'BITE',           // COCK
+  'ZIZI',           // DICK
+  'MASTURBATEUR',   // MASTURBATOR
+  'VAGIN',          // VAGINA
+  'CLITORIS',       // CLITORIS
+  'RABBIT',         // RABBIT
+  'FESSE',          // BUTT
+  'CUL',            // ASS (argot)
+  'GODE',
+  'GODES',           // DILDO
+  'PÉNIS',          // PENIS
+  'ANAL',           // ANAL
+  'ANTIBACTÉRIEN',  // ANTIBACTERIAL       
+  'URÉTHRAL',       // URETHRAL
+  'ANNEAU PÉNIEN',  // COCKRING  (déjà plus haut mais on le répète si besoin)
+  'CHIEN MALTAIS',
+  'PÉNÉTRATION',
+  'VAGINS',
+  'VAGINAL',
+  'VAGINALE',
+  'VIBROMASSEUR',
+  'CLITORIDIEN',
+  'VIBROMASSEURS',
+  'CLITORIDIENS',
+  'CLITORAL',
+  'VIBRATEUR',
+  'VIBRATEURS',
+  'ANAUX',
+  'PLUG',
+  'DONG',
+  'COCKRING',
+  'SEXUEL',
+  'SEXUELLE',
+  'SEXUELS',
+  'SEXUELLES',
+  'SEXTOYS',
+  'SENSUELLE',
+  'SENSUELLES',
+];
