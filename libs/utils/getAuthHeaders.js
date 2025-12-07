dfce5e7b82a09d81920460693891e27465934e11
VeronaDanzka <davide.denotarpietro@gmail.com>
2025-12-07 03:48:20 +0100

    changement content language et accept language pour CH

--- libs/utils/getAuthHeaders.js
+++ libs/utils/getAuthHeaders.js
@@ -21,7 +21,7 @@ export async function getAuthHeaders() {
     Authorization: `Bearer ${tokenCache.access_token}`,
     'Content-Type': 'application/json',
     Accept: 'application/json',
-    'Accept-Language': 'fr-FR',
-    'Content-Language': 'fr-FR'
+    'Accept-Language': 'de-CH',
+    'Content-Language': 'de-CH'
   };
 };
